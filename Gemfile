0d0f684d8777a8ecdf174b64cf498729eea54925
capricia karolak <capricia@karolak.fr>
2025-07-08 21:51:20 +0200

    cloudinary

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ gem 'image_processing', '~> 1.2'
 
 gem "flatpickr"
 
+gem 'cloudinary'
+
 group :development, :test do
   gem "dotenv-rails"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
+  has_one_attached :photo
   validates :title, :author, :categories, :description, presence: true
   belongs_to :user
   has_many :reviews
   has_many :rentals, dependent: :destroy
-  has_one_attached :image
 end
